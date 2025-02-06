16cff4ea95cbaa72f88f620bea343f9071358f94
ToweryA2704 <towerya2704@student.faytechcc.edu>
2025-02-06 14:20:10 -0500

    Add files via upload

---
+++ P1LAB1_ToweryAnthony.py
@@ -0,0 +1,16 @@
+ # Anthony Towery
+ # 2-6-2025
+ # P1LAB1
+ # Lab work involving Python
+
+ # Input info
+
+fname = input("Enter your first name: ")
+
+lname = input("Enter your last name: ")
+
+# Welcome message
+print("Hello, " + str(fname) + " " + str(lname) + "! Welcome to CTR-110")
+# Alternative method below
+# print("Hello,",fname, str(lname) +"! Welcome to CTI-110")
+
