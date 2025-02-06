ebeed61dca4c1a218305d64d5e1c1dbd05499043
ToweryA2704 <towerya2704@student.faytechcc.edu>
2025-02-06 14:20:30 -0500

    Add files via upload

---
+++ Mod3_CW23.py
@@ -0,0 +1,22 @@
+# Anthony Towery
+# 2-4-2025
+# M3CW23
+# In class practice
+
+# declare a variable for first name
+
+fname = input("Enter your first name: ")
+
+lname = input("Enter your last name: ")
+
+age = int(input("Enter your age: "))
+
+#num1 = float(input("Enter a number: "))
+
+print("My first name is "+ str(fname) + " and my last name is " + str(lname) + ".")
+print("My age is " + str(age) + ".")
+#print(lname)
+#print(age)
+#print(num1)
+#print("Anthony")
+#print("Towery")
