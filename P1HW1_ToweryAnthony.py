c975bc96231d55a89d73c74036c18807d6ec73ea
ToweryA2704 <towerya2704@student.faytechcc.edu>
2025-02-11 14:45:57 -0500

    Add files via upload

---
+++ P1HW1_ToweryAnthony.py
@@ -0,0 +1,38 @@
+ # Anthony Towery
+ # February 11th, 2025
+ # P1HW1
+ # A Python program that calcuates exponenets.
+ 
+#\n is the newline character
+print("\n-----Calculating Exponenets----\n\n")
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print("\n")
+value = base**exponent
+print(base,"raised to the power of",exponent,"is",value,"!!")
+print("\n\n-----Addition and Subtraction-----\n\n")
+num1 = int(input("Enter an starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+
+value2 = num1+num2-num3
+print("\n")
+print(num1,"+",num2,"-",num3,"is equal to",value2)
+
+
+
+
+
+#value = pow(base, exponent)
+#value = math.pow(base, exponent)
+#print("-----Calculating Exponenets----")
+#print("-"*20,"Calculating Exponenets","-"*20)
+#num1 = int(input("Enter a number: "))
+#num2 = int(input("Enter another number: "))
+#import math
+#num3 = num1 + num2
+#num3 = str(num3)
+
+#print(num1, "+", num2,"=", num3)
+
+#input("Press any key to continue!!")
---
+++ P1HW2_ToweryAnthony.py
@@ -0,0 +1,19 @@
+ # Anthony Towery
+ # February 11th, 2025
+ # P1HW2
+ # A Python program that uses budget calculations and travel expenses.
+
+print("This program calculates and displays travel expenses")
+budget = int(input("\nEnter Budget: "))
+destination = input("\nEnter your travel destination: ")
+gas = int(input("\nHow much do you think you will spend on gas? "))
+accomodation = int(input("\nApproximately, how much will you need for accomodation/hotel? "))
+food = int(input("\nLast, how much do you need for food? "))
+print("\n------------Travel Expenses------------")
+print("Location:",destination)
+print("Initial Budget:",budget)
+print("\nFuel:",gas)
+print("Accomodation:",accomodation)
+print("Food:",food)
+balance = budget-gas-accomodation-food
+print("\nRemaining Balance:",balance)
