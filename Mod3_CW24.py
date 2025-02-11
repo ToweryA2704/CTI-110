d210d06a8bd4a0908c5474c1f8687d1398319bee
ToweryA2704 <towerya2704@student.faytechcc.edu>
2025-02-11 14:23:21 -0500

    Add files via upload

---
+++ Mod3_CW24.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+num3 = num1 + num2
+num3 = str(num3)
+
+print(num1, "+", num2,"=", num3)
+
+input("Press any key to continue!!")
