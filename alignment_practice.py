3f9613288fa87334a0d7b9ba9c982de5725d892c
ToweryA2704 <towerya2704@student.faytechcc.edu>
2025-02-25 14:27:27 -0500

    Add files via upload

---
+++ alignment_practice.py
@@ -0,0 +1,16 @@
+first = "Anthony"
+middle = "Ronald"
+last = "Towery"
+name1 = "Alice"
+age = 20
+
+# Left alignment with width 10
+print(f"{first:#^10} is {age} years old.")
+
+
+# Left alignment with width 10
+print(f"{middle:#^10} is {age} years old.")
+
+
+# Left alignment with width 10
+print(f"{last:#^10} is {age} years old.")
