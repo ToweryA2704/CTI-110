3a347c0df4ffa39b5953f62b4dc47917ebdd4be6
ToweryA2704 <towerya2704@student.faytechcc.edu>
2025-03-04 14:00:19 -0500

    Update P2HW2_ToweryAnthony.py

--- P2HW2_ToweryAnthony.py
+++ P2HW2_ToweryAnthony.py
@@ -3,7 +3,6 @@
 # P2HW2
 # Showing a list of grades
 
-import statistics
 
 # Ask user to import their test grades
 module1 = float(input("Enter grade for Module 1: "))
@@ -13,21 +12,20 @@ module4 = float(input("Enter grade for Module 4: "))
 module5 = float(input("Enter grade for Module 5: "))
 module6 = float(input("Enter grade for Module 6: "))
 
-
 # Store info on a list
-grades = list["module1", "module2", "module3", "module4", "module5", "module6"]
+gradeList = [] # empty list
+gradeList = [module1, module2, module3, module4, module5, module6] # add variables to the list
 
-lowestgrade = min({module1, module2, module3, module4, module5, module6})
-highestgrade = max({module1, module2, module3, module4, module5, module6})
-alltogether = sum({module1, module2, module3, module4, module5, module6})
-#adverage = statistics.mean({module1, module2, module3, module4, module5, module6})
+# figure out the lowest, highest, sum, and the length
+lowest = min(gradeList)
+highest = max(gradeList)
+total = sum(gradeList)
+average = total / len(gradeList)
 
+#print the results
 print("\n ------------Results------------")
-print(f"Lowest Grade: {lowestgrade}")
-print(f"Highest Grade: {highestgrade}")
-print(f"Sum of Grades: {alltogether}")
-print(f"Average: {adverage:.2f}")
-print("----------------------------------------")
-# Pseudocode
-# Ask user to enter their grade for each module
-#
+print(f'{"Lowest Grade:":<20}{lowest}')
+print(f'{"Highest Grade:":<20}{highest}')
+print(f'{"Sum of Grades:":<20}{total}')
+print(f'{"Average:":<20}{average:.2f}')
+print('-'*40)
