d029a8e0b37c8a519d8f6ab8f1b1f54b62e920f9
ToweryA2704 <towerya2704@student.faytechcc.edu>
2025-03-13 14:51:44 -0400

    Add files via upload

---
+++ P3HW1_ToweryAnthony.py
@@ -0,0 +1,58 @@
+# I was supposed to put a comment here
+# Anthony Towery
+# March 13, 2025
+# P3HW1
+# Showing a list of grades
+
+# This program takes a number grade , determines average and displays letter grade for average.
+
+# Enter grades for six modules
+
+mod_1 = float(input("Enter grade for Module 1: "))
+mod_2 = float(input("Enter grade for Module 2: "))
+mod_3 = float(input("Enter grade for Module 3: "))
+mod_4 = float(input("Enter grade for Module 4: "))
+mod_5 = float(input("Enter grade for Module 5: "))
+mod_6 = float(input("Enter grade for Module 6: "))
+
+# add grades entered to a list
+
+grades = [mod_1, mod_2, mod_3, mod_4, mod_5, mod_6]
+# TO DO: determine lowest, highest , sum and average for grades
+
+low = min(grades)
+high = max(grades)
+total = sum(grades)
+avg = total / len(grades)
+
+#print the results
+print("\n ------------Results------------")
+print(f'{"Lowest Grade:":<20}{low}')
+print(f'{"Highest Grade:":<20}{high}')
+print(f'{"Sum of Grades:":<20}{total}')
+print(f'{"Average:":<20}{avg:.2f}')
+print('-'*40)
+
+# determine letter grade for average
+
+
+if avg >= 90:
+    print('Your grade is: A')
+else:
+    if avg >= 80:
+        print('Your grade is: B')
+    else:
+        if avg >= 70:
+            print('Your grade is: C')
+        else:
+            if avg >= 60:
+                print('Your grade is: D')
+            else:
+                if avg >= 0:
+                    print('Your grade is: F')
+    # TO DO: finish this
+
+
+
+
+
